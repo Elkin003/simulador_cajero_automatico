5ca74de134b74b1dba7f2cc3f9c5ebc0e7e2fe68
Elkin <elkinjjm@hotmail.com>
2025-06-16 14:50:26 -0500

    paquetes

---
+++ src/views/Simulador_cajero_automatico.java
@@ -0,0 +1,21 @@
+package views;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Simulador_cajero_automatico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
